7a6c3d3e78c32bc01caefe2def326a3dfa74385b
unknown <guilhermebrito@gec.inatel.br>
2025-11-30 17:16:43 -0300

    feat: testes de login

--- cypress/e2e/teste.cy.js
+++ cypress/e2e/teste.cy.js
@@ -1,8 +1,8 @@
+const randomEmail = `teste_automatico_${Date.now()}@teste.com`
+
 describe('Cadastrar Usuario', () => {
   it('tenta criar novo usuario', () => {
     cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=account/register')
-
-    const randomEmail = `teste_automatico_${Date.now()}@teste.com`
 
     cy.get('#input-firstname').type('Teste')
     cy.get('#input-lastname').type('Automatico')
@@ -16,5 +16,32 @@ describe('Cadastrar Usuario', () => {
     cy.get('input[type="submit"][value="Continue"]').click()
 
     cy.contains('Your Account Has Been Created!').should('be.visible')
+  })
+})
+
+describe('Login do usuário', () => {
+  it('tenta realizar login', () => {
+    cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=account/login')
+
+    cy.get('#input-email').type(randomEmail)
+    cy.get('#input-password').type('senha_forte_4631@')
+
+    cy.get('input[type="submit"][value="Login"]').click()
+
+    cy.contains('My Account').should('be.visible')
+
+  })
+})
+
+describe('Login com senha incorreta', () => {
+  it('testa login usando senha errada', () => {
+    cy.visit('https://ecommerce-playground.lambdatest.io/index.php?route=account/login')
+
+    cy.get('#input-email').type(randomEmail)
+    cy.get('#input-password').type('saiDaquiBill')
+
+    cy.get('input[type="submit"][value="Login"]').click()
+
+    cy.contains('Warning: No match for E-Mail Address and/or Password.').should('be.visible')
   })
 })
